5de9f5f6676ff197ebc7b60da626fbc7c31c9676
Stefanie Doll <stefie.doll@googlemail.com>
2025-11-25 11:45:46 +0100

    delete Postgesql 18 migration files

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20251124_115127 from './20251124_115127';
-
-export const migrations = [
-  {
-    up: migration_20251124_115127.up,
-    down: migration_20251124_115127.down,
-    name: '20251124_115127'
-  },
-];
